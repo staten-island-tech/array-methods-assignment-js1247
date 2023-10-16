235a41496647487aaddc6b19351c4c1135dbbe54
js1247 <jasonshi1247@gmail.com>
2023-10-15 22:29:40 -0400

    js

---
+++ app.js
@@ -0,0 +1,21 @@
+const name = [
+
+    {
+       firstName:"",
+       lastName: "",
+   },
+   
+   {
+       firstName:"",
+       lastName: "",
+   },
+   {
+       firstName:"",
+       lastName: "",
+   },
+   
+   {
+       firstName:"",
+       lastName: "",
+   }
+   ]
