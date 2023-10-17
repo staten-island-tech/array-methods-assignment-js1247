d3afd5b23b86f8d4a5eb368ae28588cb6f2e10df
js1247 <jasonshi1247@gmail.com>
2023-10-16 22:54:27 -0400

    js

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+<<<<<<< HEAD
+const Athletes = [
+  {
+    firstName: "Ryan",
+    country: "Mexico",
+    age: "26",
+    sport: "Basketball",
+    gender: "Male",
+  },
+  {
+    firstName: "Evan",
+    country: "Japan",
+    age: "29",
+    sport: "Badminton",
+    gender: "Male",
+  },
+  {
+    firstName: "Sophia",
+    country: "England",
+    age: "24",
+    sport: "Volleyball",
+    gender: "Female",
+  },
+  {
+    firstName: "Yifan",
+    country: "China",
+    age: "30",
+    sport: "Track",
+    gender: "Female",
+  },
+  {
+    firstName: "Johnny",
+    country: "Africa",
+    age: "25",
+    sport: "soccer",
+    gender: "Male",
+  },
+];
+Athletes.forEach((athletes) => {
+  console.log(athletes.firstName);
+  console.log(athletes.country);
+  console.log(athletes.age);
+  console.log(athletes.sport);
+  console.log(athletes.gender);
+});
+function genderFiltering() {
+  return Athletes.filter((number) => number.gender == "Male");
+}
+console.log(genderFiltering());
+=======
 const name = [
 
     {
@@ -23,4 +73,9 @@ const name = [
        lastName: "",
        phoneNumber: ""
    }
+<<<<<<< Updated upstream
    ];
+=======
+   ];
+>>>>>>> f610e0ab1cb6f58919eb3b4257eda2862284c8c9
+>>>>>>> Stashed changes
